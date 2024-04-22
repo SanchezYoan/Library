cc20d63e38617c2afa417954b9cb42c65cf61394
SanchezYoan <yoan30470@hotmail.fr>
2024-04-22 16:23:16 +0200

    start

---
+++ library/src/components/Library.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Library = () => {
+    return (
+        <div>
+            <h1>Books</h1>
+            <div>
+                <button>Ajouter un nouveau livre</button>
+
+                <form>
+                    <div>
+
+                        <label htmlFor="name">Titre</label>
+                        <input
+                            type="text"
+                            id="name"
+                        />
+                    </div>
+                    <div>
+
+                        <label htmlFor="genre">Genres</label>
+                        <input
+                            type="text"
+                            id="genre"
+                        />
+                    </div>
+
+                    <div>
+
+                        <label htmlFor="Auteur">Auteur</label>
+                        <input
+                            type="text"
+                            id="Auteur"
+                        />
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Library;
---
+++ library/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Library from '../components/Library';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <Library />
+        </div>
+    );
+};
+
+export default Home;
