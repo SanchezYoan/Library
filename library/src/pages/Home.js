0566ed0e59734d8e867857895866888381362294
SanchezYoan <yoan30470@hotmail.fr>
2024-04-26 14:57:34 +0200

    styles

--- library/src/pages/Home.js
+++ library/src/pages/Home.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Collection from '../components/Collection';
 import Book from '../components/Book';
 
 const Home = () => {
+    const [isCollection, setIsCollection] = useState(false)
     return (
-        <div>
-            <h1>Bienvenue sur Library</h1>
-            <h2>Que voulez vous faire aujourd'hui ?</h2>
-            <div>
-                <button>Voir la collection</button>
+        <div className='homeContainer'>
+            <div className='headerContainer'>
+                <h1>Bienvenue sur Library</h1>
+                <h2>Que voulez vous faire aujourd'hui ?</h2>
+            </div>
+            <div className='actionContainer'>
+                <button onClick={() => setIsCollection(!isCollection)}>Voir la collection</button>
                 <button>Ajouter un livre</button>
                 <button>Ajouter un autheur</button>
             </div>
-
-            <Book />
+            {
+                isCollection ? (
+                    <Book />
+                ) : (
+                    <></>
+                )
+            }
         </div>
     );
 };
