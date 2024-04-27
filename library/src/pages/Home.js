54bdca1a89ee58239e3634586ed373392b65d592
SanchezYoan <77174458+SanchezYoan@users.noreply.github.com>
2024-04-27 18:10:03 +0200

    Feature 002/collection (#2)
    * styles
    
    * collection style

--- library/src/pages/Home.js
+++ library/src/pages/Home.js
@@ -1,19 +1,26 @@
-import React from 'react';
-// import Collection from '../components/Collection';
-import Book from '../components/Book';
+import React, { useState } from 'react';
+import Collection from '../components/Collection';
 
 const Home = () => {
+    const [isCollection, setIsCollection] = useState(false)
     return (
-        <div>
-            <h1>Bienvenue sur Library</h1>
-            <h2>Que voulez vous faire aujourd'hui ?</h2>
-            <div>
-                <button>Voir la collection</button>
+        <div className='homeContainer'>
+            <div className='headerContainer'>
+                <h1>Bienvenue sur Library</h1>
+                <h2>Que voulez vous faire aujourd'hui ?</h2>
+            </div>
+            <div className='actionContainer'>
+                <button onClick={() => setIsCollection(!isCollection)}>Voir la collection</button>
                 <button>Ajouter un livre</button>
                 <button>Ajouter un autheur</button>
             </div>
-
-            <Book />
+            {
+                isCollection ? (
+                    <Collection />
+                ) : (
+                    <></>
+                )
+            }
         </div>
     );
 };
