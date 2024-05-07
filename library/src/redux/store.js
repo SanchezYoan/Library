87ed5b11b5f6ba59f3373e3a205b73c140e88dc7
SanchezYoan <yoan30470@hotmail.fr>
2024-05-07 14:39:34 +0200

    store

--- library/src/redux/store.js
+++ library/src/redux/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
-import reducerCourses from "./reducers/reducerCourses";
 import reducerCart from "./reducers/reducerCart";
-import reducerPayment from "./reducers/reducerPayment";
 
 const rootReducer = combineReducers({
   books: reducerBooks,
